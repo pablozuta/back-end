6cb46d7b26c4c36cdfc57c081165a411ef3d6ad0
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-03-02 03:16:39 -0400

    se agrega funcion a middleware proyecto express server 3

--- express-server-03/main.js
+++ express-server-03/main.js
@@ -4,20 +4,29 @@ const app = express()
 // enable middleware para acceder a los datos que recibimos de form
 app.use(express.urlencoded({ extended: false }))
 
+function getWeather(request, response, next) {
+    request.visitorWeather = true
+    next()
+} 
+
+
 // routes para get requests
-app.get('/', (request, response) => {
+app.get('/', getWeather , (request, response) => {
   response.send(`
     <style>
     body {background-color: powderblue; }
     input {padding:0.5rem ; width:400px;}
+    button {padding:0.5rem;}
    
     </style>
     <h1>What Color is the sky on a clean day? </h1>
-
+    
     <form action="/result" method="POST">
     <input type="text" name="color" placeholder="Enter a Color">
     <button>Submit Answer </button>
     </form>
+
+    <p> ${request.visitorWeather ? 'It is Raining' : 'Not Raining Bro'}</p>
     `)
 })
 
@@ -34,7 +43,15 @@ app.post('/result', (request, response) => {
     </style>
     <h1>Yes, the Sky is Blue 👽</h1>
     `)
-  } else {
+  } else if(request.body.color.trim().toLowerCase() === '') {
+    response.send(`
+    <style>
+    body {background-color: pink; }
+    </style>
+    <h1>Please enter a color</h1>
+    
+    `)
+  }  else {
     response.send(`
         <style>
         body {background-color: tomato; }
